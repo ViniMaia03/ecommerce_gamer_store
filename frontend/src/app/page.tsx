d92705b955462c71f18db6e494d1101d5b46a5d7
Vinicius <viniciusmsantos02@gmail.com>
2024-10-01 20:10:26 -0300

    configurações das imagens

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,12 +4,14 @@ import { produtos } from "@/core";
 export default function Home() {
   return (
     <>
-      <ProdutoItem produto = { produtos[0] } />
-      <ProdutoItem produto = { produtos[1] } />
-      <ProdutoItem produto = { produtos[2] } />
-      <ProdutoItem produto = { produtos[3] } />
-      <ProdutoItem produto = { produtos[4] } />
-      <ProdutoItem produto = { produtos[5] } />
+      <div className="flex gap-5 flex-wrap">
+        <ProdutoItem produto = { produtos[0] } />
+        <ProdutoItem produto = { produtos[1] } />
+        <ProdutoItem produto = { produtos[2] } />
+        <ProdutoItem produto = { produtos[3] } />
+        <ProdutoItem produto = { produtos[4] } />
+        <ProdutoItem produto = { produtos[5] } />
+      </div>
     </>
   )
 }
--- frontend/src/components/produto/produtoItem.tsx
+++ frontend/src/components/produto/produtoItem.tsx
@@ -1,5 +1,6 @@
 import { Produto, produtos } from "@/core"
 import Link from "next/link"
+import Image from "next/image"
 
 export interface ProdutoItemProps {
     produto: Produto
@@ -8,6 +9,15 @@ export interface ProdutoItemProps {
 export default function ProdutoItem(props: ProdutoItemProps) {
     const {produto} = props
     return(
-        <Link href={`/produto/${produto.id}`}>{props.produto.nome}</Link>
+        <Link href={`/produto/${produto.id}`} 
+        className="flex flex-col bg-violet-dark border border-white/10 rounded-xl relative max-w-[350px]">
+            <div className="h-48 w-full relative">
+                <Image src= 
+                    {produto.imagem} 
+                    fill 
+                    alt='Imagem do Produto' />
+            </div>
+            {props.produto.nome}
+        </Link>
     )
 }
